31b8f1092dd7b5b02e482eb81d20bf2158e6060d
Fabian Huch <huch@in.tum.de>
2021-07-01 11:28:15 +0200

    Added PAM auth for isabelle users

--- ansible/roles/jenkins/templates/security.groovy
+++ ansible/roles/jenkins/templates/security.groovy
@@ -1,8 +1,27 @@
+import com.michelin.cio.hudson.plugins.rolestrategy.RoleBasedAuthorizationStrategy
+import com.michelin.cio.hudson.plugins.rolestrategy.Role
+import com.synopsys.arc.jenkins.plugins.rolestrategy.RoleType
+
 org.jenkinsci.main.modules.sshd.SSHD.get().setPort(-1)
 
-realm = new hudson.security.HudsonPrivateSecurityRealm(false, false, null)
+realm = new hudson.security.PAMSecurityRealm("sshd")
 
-strategy = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
+strategy = new RoleBasedAuthorizationStrategy()
+
+// Create roles
+adminRole = new Role('admin', hudson.security.Permission.getAll().toSet())
+buildRole = new Role('build', [hudson.model.Item.BUILD, hudson.model.Item.CANCEL] as Set)
+readRole = new Role('read', [jenkins.model.Jenkins.READ, hudson.model.Item.READ, hudson.model.Item.DISCOVER] as Set)
+strategy.addRole(RoleType.Global, adminRole)
+strategy.addRole(RoleType.Global, buildRole)
+strategy.addRole(RoleType.Global, readRole)
+
+// Assign roles to users/groups
+strategy.assignRole(RoleType.Global, adminRole, 'admin')
+strategy.assignRole(RoleType.Global, readRole, 'isabelle')
+strategy.assignRole(RoleType.Global, buildRole, 'isabelle')
+strategy.assignRole(RoleType.Global, readRole, 'anonymous')
+
 
 crumbs = new hudson.security.csrf.DefaultCrumbIssuer(false)
 
